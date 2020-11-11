4abf0b253804912c4b18caf4daf89bf009678d26
Jorge Calatayud Maeso <jorge.cmaeso@alumnos.upm.es>
2020-11-12 00:31:35 +0100

    Updated with new folder structure

---
+++ remove_folder.sh
@@ -0,0 +1,7 @@
+git filter-branch --tree-filter "rm -rf cst" --prune-empty HEAD
+git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
+echo node_modules/ >> .gitignore
+git add .gitignore
+git commit -m 'Removing node_modules from git history'
+git gc
+git push origin master --force
